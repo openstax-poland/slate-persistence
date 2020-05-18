345baad439dc51beddda111fc1acf76281a8b915
Krzysztof Mędrzycki <aiwenari@gmail.com>
2020-05-18 16:33:51 +0200

    Export API and types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 // Copyright 2020 OpenStax Poland
 // Licensed under the MIT license. See LICENSE file in the project root for
 // full license text.
+
+export { DocumentDB, PersistDB } from './database'
+export { PersistingEditor } from './persisting-editor'
+export { Export } from './util'
+export { default as withPersistence } from './with-persistence'
